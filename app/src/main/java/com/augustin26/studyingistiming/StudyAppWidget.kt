54e6ab5b4bd271f49ee66d8b2c4ddfa3a156fe29
leegyeongjun <augustin26@e9pay.co.kr>
2021-11-05 18:19:47 +0900

    [Refactor]

--- app/src/main/java/com/augustin26/studyingistiming/StudyAppWidget.kt
+++ app/src/main/java/com/augustin26/studyingistiming/StudyAppWidget.kt
@@ -1,12 +1,21 @@
 package com.augustin26.studyingistiming
 
+import android.R
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.widget.RemoteViews
 
+
 class StudyAppWidget : AppWidgetProvider() {
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -20,17 +29,25 @@ class StudyAppWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    internal fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val widgetText = context.getString(com.augustin26.studyingistiming.R.string.appwidget_text)
+        // Construct the RemoteViews object
+        val views = RemoteViews(
+            context.packageName,
+            com.augustin26.studyingistiming.R.layout.new_app_widget
+        )
+        // Construct an Intent object includes web adresss.
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"))
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        // Here the basic operations the remote view can do.
+        views.setOnClickPendingIntent(com.augustin26.studyingistiming.R.id.btnStart, pendingIntent)
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
 }
